a39c004e3f142f1c6f24734e8c0733af84e5dfe8
JboufkriLab <j.boufkri@gmail.com>
2023-04-11 13:16:29 +0200

    bouton

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -163,21 +163,13 @@ const Header = () => {
                   : " border-transparent ")
               }
             >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              Plus
+               <button type="button" class="bg-white text-slate-900 dark:bg-slate-100 dark:text-slate-700 flex-none -my-2 mx-auto w-20 h-20 rounded-full ring-1 ring-slate-900/5 shadow-md flex items-center justify-center" aria-label="Pause">
+    <svg width="30" height="32" fill="currentColor">
+      <rect x="6" y="4" width="4" height="24" rx="2" />
+      <rect x="20" y="4" width="4" height="24" rx="2" />
+    </svg>
+  </button>
+               
             </LinkScroll>
             <LinkScroll
               activeClass="active"
